724d52cb4e5662a3ab3579f50c80d90d9e8b7fb7
LeonSagebrand <144245796+LeonSagebrand@users.noreply.github.com>
2024-08-27 14:14:59 +0200

    Update api.js

--- vite-project/src/api/api.js
+++ vite-project/src/api/api.js
@@ -1,5 +1,5 @@
 export async function fetchApi (setStateVariable) {
-    const response = await fetch("https://openexchangerates.org/api/latest.json?app_id=2c5ad271c49c412887a55d886e14ecde");
+    const response = await fetch("https://openexchangerates.org/api/latest.json?app_id=");
     const data = await response.json();
 
     return setStateVariable(data)
